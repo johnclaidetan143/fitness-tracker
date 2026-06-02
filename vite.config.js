71b3c0293946bae14ca65ae79e4122c0dbc048e1
johnclaidetan143 <johnclaidetan143@gmail.com>
2026-06-02 16:05:04 +0800

    fix git submodules

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
-// vite disabled for vercel deployment
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
