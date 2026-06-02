273e6d68e189169dffe2d3315b5773f6e1c328cf
johnclaidetan143 <johnclaidetan143@gmail.com>
2026-06-02 15:46:18 +0800

    disable vite for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,1 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
-});
+// vite disabled for vercel deployment
